cd45db91027dd2008ae15b3715b0c969fcefafe6
sarahjane2021 <ycotjane722@gmail.com>
2025-02-21 08:59:42 +0800

    test

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ from sqlalchemy.orm import sessionmaker
 import datetime
 
 ######################################
-
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 ####################################
 # Load custom CSS
